03599e051505890354325b6f3a91a458534289b1
akoskaaa <akos.hochrein@prezi.com>
2015-12-12 15:40:09 +0100

    minor refactor on day 3

--- day-3.py
+++ day-3.py
@@ -2,26 +2,11 @@ from collections import defaultdict
 from pprint import pprint
 
 
-def calculate_houses_visited(houses_visited, path):
-    current_location = [0, 0]
-    houses_visited[','.join(map(str, current_location))] += 1
-    for d in path:
-        if d == '^':
-            current_location[1] += 1
-        if d == '>':
-            current_location[0] += 1
-        if d == 'v':
-            current_location[1] -= 1
-        if d == '<':
-            current_location[0] -= 1
-        houses_visited[','.join(map(str, current_location))] += 1
-    return houses_visited
-
-def calculate_houses_visited_with_robo_santa(houses_visited, path):
+def calculate_houses_visited(houses_visited, path, with_robo=False):
     current_location = {'robo': [0, 0], 'real': [0, 0]}
     houses_visited[','.join(map(str, current_location['real']))] += 1
     for i, d in enumerate(path):
-        current_santa = 'robo' if i % 2 else 'real'
+        current_santa = 'robo' if with_robo and i % 2 else 'real'
         if d == '^':
             current_location[current_santa][1] += 1
         if d == '>':
@@ -32,7 +17,6 @@ def calculate_houses_visited_with_robo_santa(houses_visited, path):
             current_location[current_santa][0] -= 1
         houses_visited[','.join(map(str, current_location[current_santa]))] += 1
     return houses_visited
-
 
 
 directions = ''
@@ -46,5 +30,5 @@ print len(coords_visited.keys())
 
 # 2
 coords_visited = defaultdict(int)
-coords_visited = calculate_houses_visited_with_robo_santa(coords_visited, directions)
+coords_visited = calculate_houses_visited(coords_visited, directions, with_robo=True)
 print len(coords_visited.keys())
