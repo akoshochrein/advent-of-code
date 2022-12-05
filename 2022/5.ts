07f61a4ff52858f628cb5da2ceacb52538aa820d
Akos Hochrein <hoch.akos@gmail.com>
2022-12-05 23:34:05 +0100

    2022 5 parser

--- 2022/5.ts
+++ 2022/5.ts
@@ -1,4 +1,9 @@
-const rawStacks = ["ZN", "MCD", "P"];
+import { SlowBuffer } from "buffer";
+
+const rawStacks = `    [D]    
+[N] [C]    
+[Z] [M] [P]
+ 1   2   3 `;
 const input = `move 1 from 2 to 1
 move 3 from 1 to 3
 move 2 from 2 to 1
@@ -34,7 +39,21 @@ const concatenateTopCrates = (topCrates: string[], stack: string[]) => [
     stack.length > 0 ? stack[stack.length - 1] : "",
 ];
 
-const stacks = rawStacks.map((rs) => rs.split(""));
+const columns = rawStacks
+    .split("\n")
+    .map((l) => l.split(""))
+    .map((l) => l.filter((_, i) => i % 2 !== 0))
+    .map((l) => l.filter((_, i) => i % 2 === 0));
+
+const stacks = columns[0]
+    .map((_, c) => columns.map((_, r) => columns[r][c]))
+    .map((stack) => stack.reverse())
+    .map((stack) => stack.filter((crate) => crate !== " "))
+    .map((stack) => {
+        const [_, ...rest] = stack;
+        return rest;
+    });
+
 const result = input
     .split("\n")
     .map(toInstruction)
